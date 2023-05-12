73a9b38502a34c14c14ce94d6e8cf9640124b571
Adam Hoyle <adam@atommacbook2023.local>
2023-05-12 15:18:29 +0100

    Allow for multiple times

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 
 function calculate() {
-  const cursor = document.getElementById("cursorPosition").value
   const maxTime = document.getElementById("maxTime").value
   const totalBeats = document.getElementById("totalBeats").value
-  const value = convertValue(cursor, maxTime, totalBeats)
-  document.getElementById("answer").innerText = value
+  const positions = document.getElementById("cursorPositions").value
+  const calculated = [];
+  positions.split("\n").forEach((position, index) => {
+    const value = convertValue(position, maxTime, totalBeats);
+    calculated.push(value);
+  })
+  //
+  document.getElementById("answer-raw").innerText = calculated.join("\n");
+  document.getElementById("answer-rounded").innerText = calculated.map(value => Math.round(value).toString(10)).join("\n");
 }
 
 const convertValue = function(cursorPosition, maxTime, totalBeats) {
   const pos = convert(toArrayOfNumbers(cursorPosition))
+  // const max = convert(additionOnArray(toArrayOfNumbers(maxTime),0,-1))
   const max = convert(toArrayOfNumbers(maxTime))
   return totalBeats - ((pos/max) * totalBeats)
 }
 
 const convert = function(arr) {
-  return arr[0] + (arr[1] / 4) + (arr[2]/255/4)
+  return (arr[0]-1) + (arr[1] / 4) + (arr[2]/255/4)
+}
+
+const additionOnArray = function(arr,pos,increment) {
+  if (pos < arr.length) {
+    arr[pos] += increment
+  }
+  return arr
 }
 
 const toArrayOfNumbers = function(incoming) {
